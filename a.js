48d2a9cc1ad9d69a354b45099849eb1d6b2aa6b5
Jingtian <3429056102@qq.com>
2020-01-07 19:19:41 +0800

    2

--- a.js
+++ a.js
@@ -1,1 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-07 19:08:46
+ * @LastEditTime : 2020-01-07 19:19:34
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /test/a.js
+ */
 var  a=1;
+var b = 1;
